41758b56b7edd2ecdfd718c6491a2d28b0b0a854
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-05-15 17:31:29 +0300

    return brand

--- src/components/pages/palettePage/containers/ThreePalettesContainer.js
+++ src/components/pages/palettePage/containers/ThreePalettesContainer.js
@@ -2,18 +2,21 @@ import {Grid} from "@mui/material";
 import {PaletteBox} from "./PaletteBox";
 import popupDialog from "../ConfirmationPopupDialog";
 import ConfirmationPopupDialog from "../ConfirmationPopupDialog";
-import React from "react";
+import React, {useEffect} from "react";
 
 const ThreePalettesContainer = ({threePalettesArray, popupDeleteDialog, handleEdit, onDelete}) => {
+    useEffect(() => {
+        console.log(threePalettesArray)
+    }, [threePalettesArray]);
 
     return (
 
         <Grid  container spacing={2} mb={2} paddingBottom={4} style={{borderBottom: 'solid', borderColor: '#C0C0C0'}}>
 
-            {threePalettesArray.map(i => {
-                return <Grid  item xs={4} key={i.id}>
+            {threePalettesArray.map((i, k) => {
+                return <Grid  item xs={4} key={k}>
                     <PaletteBox
-                        brand = { i.brandDto }
+                        brand = { i.brand }
                         name={i.name}
                         description={i.description}
                         image={i.image}
